12846c8b9084b7c8539441f22148112dba5a857f
bandicode <vincent.chambrin@gmail.com>
2020-12-27 13:49:34 +0100

    improve callstack view

--- tools/gonkdbg/callstackview.cpp
+++ tools/gonkdbg/callstackview.cpp
@@ -10,11 +10,16 @@
 
 #include <QDebug>
 
+static constexpr int NUMBER_COLUMN = 0;
+static constexpr int FUNCTION_COLUMN = 1;
+static constexpr int FILE_COLUMN = 2;
+static constexpr int LINE_COLUMN = 3;
+
 CallstackView::CallstackView(Controller& c)
   : m_controller(c)
 {
-  setColumnCount(3);
-  setHeaderLabels(QStringList() << "Function" << "File" << "Line");
+  setColumnCount(4);
+  setHeaderLabels(QStringList() << "#" << "Function" << "File" << "Line");
 
   connect(&m_controller, &Controller::callstackUpdated, this, &CallstackView::onCallstackUpdated);
 }
@@ -25,18 +30,23 @@ void CallstackView::onCallstackUpdated()
 
   clear();
 
-  for (const gonk::debugger::CallstackEntry& e : callstack.entries)
+  int d = callstack.entries.size();
+
+  for (auto it = callstack.entries.rbegin(); it != callstack.entries.rend(); ++it)
   {
+    const gonk::debugger::CallstackEntry& e = *it;
+
     QTreeWidgetItem* item = new QTreeWidgetItem;
     item->setFlags(Qt::ItemFlag::ItemIsEnabled | Qt::ItemFlag::ItemNeverHasChildren);
 
-    item->setText(0, QString::fromStdString(e.function));
-    item->setText(1, QString::fromStdString(e.path));
+    item->setText(NUMBER_COLUMN, QString::number(d--));
+    item->setText(FUNCTION_COLUMN, QString::fromStdString(e.function));
+    item->setText(FILE_COLUMN, QString::fromStdString(e.path));
 
     if (e.line != -1)
     {
       item->setFlags(item->flags() | Qt::ItemFlag::ItemIsSelectable);
-      item->setText(2, QString::number(e.line + 1));
+      item->setText(LINE_COLUMN, QString::number(e.line + 1));
     }
 
     addTopLevelItem(item);
